bdefbe59796af0a15965406f87a180200403b010
ivanjelisavcic <ivan.jelisavcic@globalkom.com>
2025-06-26 15:49:00 +0200

    inicijalizovan redux store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideRedux } from '@reduxjs/angular-redux';
-import { store } from './store';
+import  store  from './store';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideRedux({ store })]
